5d5072d58b9c0c9a7c94f41cd31b11081c8706e8
imredshadow <jianturalbaacol@gmail.com>
2025-09-03 18:18:46 +0000

    initial commit

--- Easter.java
+++ Easter.java
@@ -1,1 +1,8 @@
+//Jian Acol
+//This program will calculate the date easter falls on given a year after 1853
+public class Easter{
+    public static void main(String[] args) {
+        //code goes here
+    }
 
+}
