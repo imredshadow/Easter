7e57a2adbd766ecb7d326aea3c6bef9e08a408f5
imredshadow <jianturalbaacol@gmail.com>
2025-09-03 18:29:57 +0000

    67

--- Easter.java
+++ Easter.java
@@ -1,8 +1,14 @@
 //Jian Acol
-//This program will calculate the date easter falls on given a year after 1853
+//This program will calculate the date easter falls on given a year after 1583
 public class Easter{
     public static void main(String[] args) {
         //code goes here
+        int y = 1583;
+        System.out.println("y = "+y);
+        int a = y%19;
+        System.out.println("a = "+a);
+        int b = y/100;
+        System.out.println("b = "+b);
     }
 
 }
