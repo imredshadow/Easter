1c4820e8238ef1b91724f87927c5ff5a19044f4c
imredshadow <jianturalbaacol@gmail.com>
2025-09-05 17:08:39 +0000

    67

--- Easter.java
+++ Easter.java
@@ -3,7 +3,7 @@
 public class Easter{
     public static void main(String[] args) {
         //code goes here
-        int y = 1583;
+        int y = 2003;
         int a = y%19;
         System.out.println("a = "+a);
         int b = y/100;
@@ -12,13 +12,13 @@ public class Easter{
         System.out.println("c = "+c);
         int d = b/4;
         System.out.println("d = "+d);
-        int e = b/4;
+        int e = b%4;
         System.out.println("e = "+e);
         int f = (b+8)/25;
         System.out.println("f = "+f);
         int g = ((b-f)+1)/3;
         System.out.println("g = "+g);
-        int h = (19*a+b-d-g+15)*30;
+        int h = (19*a+b-d-g+15)%30;
         System.out.println("h = "+h);
         int i = c/4;
         System.out.println("i = "+i);
@@ -33,5 +33,4 @@ public class Easter{
         int p = (h+r-7*m+114)%31;
         System.out.println("p = "+p);
     }
-
 }
